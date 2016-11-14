da73d75453371e732b4311e4621813f25817d25a
Asma Sattar <asmasattar@Asmas-MacBook-Pro.local>
2016-11-13 22:40:47 -0500

    Worked on server side flagging points
    https://github.com/Tour-de-Force/btc-app-server/issues/36

--- src/flag.js
+++ src/flag.js
@@ -16,13 +16,53 @@
  * You should have received a copy of the Affero GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
+import { PointCollection } from 'btc-models';
 
-export function list( req, res ) {
-  res.json( [ {
-    id: 1,
-    flag: true
-  }, {
-    id: 2,
-    flag: true
-  } ] );
+export default function flag( req, res ) {
+  const {pointId} = req.body;
+
+  if ( pointId === '' || pointId === undefined ) {
+    return res.status( 400 ).json( {
+      'bad request': 'You must provide a point id.'
+    } );
+  }
+
+  // Check that the pointId is valid
+	new PointCollection().fetch({
+		success: ( points, response, options ) => {
+			const point = points.findWhere( { _id: pointId } );
+			if ( !point ) {
+				// Point they are trying to comment on doesn't exist
+				return res.status( 400 ).json( { error: "Specified point doesn't exist"} );
+			}
+			/*Add the user name to the array flagged_by*/
+			var cur_flagged_by = point.get('flagged_by');
+
+			if(cur_flagged_by.indexOf(req.user.email) > -1){
+				return res.status( 400 ).json( { error: "You've already flagged this point"} );
+			}
+
+			cur_flagged_by.push(req.user.email);
+			point.set('flagged_by', cur_flagged_by);
+			point.set('updated_by', req.user.email);
+
+			// Save comment into the database
+			point.save( {}, {
+				force: true,
+
+				success: ( comment, response, options ) => {
+					// Comment saved successfully
+					return res.status( 200 ).end();
+				},
+
+				error: ( comment, response, options ) => {
+					// Couldn't save comment
+					return res.status( 400 ).json( { error: response.message } );
+				}
+			});
+		},
+
+		// Couldn't fetch points
+		error: ( users, response, options ) => res.status( 500 ).end()
+	});
 }
